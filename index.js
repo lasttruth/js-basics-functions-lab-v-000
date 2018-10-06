b0caaf6f5b486f68588f792c95a1e41daf2e9c21
Robin Doirin <robindoirin@gmail.com>
2018-10-06 19:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks) {
-  
+
 }
 
 function distanceTravelledInFeet(feet) {
   if (someValue <= 400) {
     return
   }
+}
+
+function distanceFromHqInFeet(feet) {
+
 }
