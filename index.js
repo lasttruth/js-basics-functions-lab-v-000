4379e28589be55ec3d6fb489e36d6a918b780238
Robin Doirin <robindoirin@gmail.com>
2018-10-06 19:09:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(feet) {
+  let result;
+if (feet <= 400) {
+    result = 'This one is on me!'} else if (feet > 2000 && feet < 2500 ) {
+        result = 'I will gladly take your thirty bucks.'
+            } else {
+                result = 'No can do.'
+                }
+    return result;
+}
