998b830dcd57f677fad2ce76f6dc081c2fcbf7f8
Robin Doirin <robindoirin@gmail.com>
2018-10-06 19:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(feet) {
   let result;
+
 if (feet <= 400) {
     result = 'This one is on me!'} else if (feet > 2000 && feet < 2500 ) {
         result = 'I will gladly take your thirty bucks.'
